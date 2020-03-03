d84e40909ff6ec778fc65deb0b56dba98324c146
Indraraj <indraraj@infiniteit.biz>
2020-03-03 11:36:27 +0530

    master to changeref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng9-repro';
+  title = "ng9-repro";
+  public myForm: FormGroup;
+
+  constructor(private swUpdate: SwUpdate) {
+    this.myForm = new FormGroup(
+      {
+        test: new FormControl({ value: "indra", disabled: false }, [
+          Validators.required
+        ])
+      },
+      { updateOn: "blur" }
+    );
+    // console.log(this.myForm.getRawValue());
+    // console.log(this.myForm.value);
+    // this.myForm.get("test").disable();
+    // this.myForm.get("test").patchValue("indraraj");
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("New version available. Load New Version?")) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PlacesModule } from "./places/places.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PlacesModule],
+  imports: [BrowserModule, AppRoutingModule, PlacesModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
